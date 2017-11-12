8042cb166ee941d01871d8d3038683f75337f60f
end <end@wheatly>
2017-11-12 22:50:57 +0100

    * initial commit

---
+++ yolobot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import time
+import telepot
+from telepot.loop import MessageLoop
+import requests
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type,chat_type,chat_id)
+
+    if content_type == 'text' and msg['text'] == '/yolo':
+        r = requests.get('https://get.yolo.jetzt')
+        bot.sendMessage(chat_id, r.text)
+
+TOKEN = sys.argv[1]
+
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+
+while 1:
+    time.sleep(10)
